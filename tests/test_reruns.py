f8863b6226661e207a8171f26319467b0680fc8d
bogdanshcherban8 <bogdanshcherban8@gmail.com>
2025-07-13 22:04:36 +0400

    Rerun

---
+++ tests/test_reruns.py
@@ -0,0 +1,17 @@
+import pytest
+import random
+Platform = 'linux'
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
+def test_reruns():
+    assert random.choice([True, False])
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
+class TestReruns:
+    def test_rerun_1(self):
+        assert random.choice([True, False])
+
+    def test_rerun_2(self):
+        assert random.choice([True, False])
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2, condition=Platform=='linux')
+def test_rerun_with_condition():
+    assert random.choice([True, False])
