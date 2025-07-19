d57741b634941e7ab0c84385fd21028d80cef353
bogdanshcherban8 <bogdanshcherban8@gmail.com>
2025-07-19 10:17:34 +0400

    version change8

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,7 +1,7 @@
 from config import settings
 
 def create_allure_environment_file():
-    items = [f'{key}={value}'for key, value in settings.model_dumb().items()]
+    items = [f'{key}={value}'for key, value in settings.model_dump().items()]
     properties = '\n'.join(items)
     with open(settings.allure_results_dir.joinpath("environment_properties"), 'w+') as file:
         file.write(properties)
