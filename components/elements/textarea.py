96dd2caa9916ef017b60b87365b32ca051f947ce
bogdanshcherban8 <bogdanshcherban8@gmail.com>
2025-07-19 13:26:14 +0400

    Coverage

--- components/elements/textarea.py
+++ components/elements/textarea.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Locator, expect
 import allure
+from ui_coverage_tool import ActionType
+
 from components.elements.base_element import BaseElement, logger
 
 
@@ -9,16 +11,20 @@ class Textarea(BaseElement):
         return "textarea"
     def get_locator(self,nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(**kwargs).locator('textarea').first
+
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(nth, **kwargs)}//textarea[1]'
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(f'Fill {self.type_of} "{self.name}" to value "{value}"'):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
-
+        self.track_coverage(action_type=ActionType.FILL, **kwargs)
     def check_have_value(self,value: str, nth: int = 0, **kwargs):
         step = f'Checking that "{self.type_of}" "{self.name}" has a value "{value}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+        self.track_coverage(action_type=ActionType.VALUE, **kwargs)
