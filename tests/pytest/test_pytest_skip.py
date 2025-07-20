131bd2e4db5319ac41f93b46698074065dbf61c2
bogdanshcherban8 <bogdanshcherban8@gmail.com>
2025-07-20 12:36:02 +0400

    Final

--- tests/pytest/test_pytest_skip.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-@pytest.mark.skip(reason="Фича в разработке")
-
-def test_features_in_development_skip():
-    ...
--- tests/pytest/test_pytest_xfail.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-@pytest.mark.xfail(reason="один не равно два")
-def test_with_bug():
-    assert 1==2
-@pytest.mark.xfail(reason='баг исправлен')
-def test_without_bug():
-    ...
